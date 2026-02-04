d6f1973942d19ada1d3b0c9ddc7f016617b19a24
highghlow <highghlow@proton.me>
2026-02-04 05:32:42 +0000

    Added parser and simplified instruction set

--- asm.py
+++ asm.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+import itertools
 
 class Instruction: ...
 
 class Register:
     def __init__(self, num):
         self.num = num
+    def is_immediate(self): return False
 
     def to(self):
         if self.num > 0:
@@ -37,57 +39,91 @@ class Register:
             return "S"
         return f"R{self.num}"
 
-def reg(n): return Register(n)
+ROOT = Register(0)
+
+regs = {
+    "R1": Register(1),
+    "R2": Register(2),
+    "R3": Register(3),
+    "R4": Register(4),
+    "R5": Register(5),
+    "R6": Register(6),
+    "R7": Register(7),
+    "R8": Register(8)
+}
+
+class Immediate(int):
+    def is_immediate(self): return True
+
+RegisterOrImmediate = Register | int
 
 @dataclass
 class LabelDefine(Instruction):
     name: str
 
+MNEMONICS = dict()
+
 @dataclass
 class Jump(Instruction):
     target: str
+MNEMONICS["jmp"] = Jump
 
 @dataclass
 class JumpConditional(Instruction):
     cond: Register
     target: str
+MNEMONICS["jnz"] = JumpConditional
 
 @dataclass
 class JumpRelative(Instruction):
     offset: int
+MNEMONICS["jmr"] = JumpRelative
 
 @dataclass
-class AddI(Instruction):
-    reg: Register
-    val: int
+class Move(Instruction):
+    dst: Register
+    src: RegisterOrImmediate
+MNEMONICS["mov"] = Move
 
 @dataclass
-class SubI(Instruction):
-    reg: Register
-    val: int
-
-@dataclass
-class SetI(Instruction):
-    reg: Register
-    val: int
+class Copy(Instruction):
+    dst: Register
+    src: Register
+MNEMONICS["cpy"] = Copy
 
 @dataclass
 class MovAdd(Instruction):
     dst: Register
-    src: Register
+    src: RegisterOrImmediate
+MNEMONICS["addm"] = MovAdd
 
 @dataclass
-class CopyAdd(Instruction):
+class Add(Instruction):
     dst: Register
     src: Register
+MNEMONICS["add"] = Add
+
+@dataclass
+class MovSub(Instruction):
+    dst: Register
+    src: RegisterOrImmediate
+MNEMONICS["subm"] = MovSub
+
+@dataclass
+class Sub(Instruction):
+    dst: Register
+    src: Register
+MNEMONICS["sub"] = Sub
 
 @dataclass
 class Raw(Instruction):
     code: str
+MNEMONICS["raw"] = Raw
 
 @dataclass
 class Output(Instruction):
-    reg: Register
+    reg: RegisterOrImmediate
+MNEMONICS["out"] = Output
 
 @dataclass
 class Block(Instruction):
@@ -151,12 +187,15 @@ def sanitize(name):
         .replace("#", "_")
     return name
 
-def move(src, *dsts, root=reg(0)):
+def move(src, *dsts, root=ROOT, negative=False):
+    if not isinstance(negative, list):
+        negative = itertools.repeat(negative)
+
     out = src.to_rel(root)+"["
     cur_reg = src
-    for dst in dsts:
+    for dst, neg in zip(dsts, negative):
         out += dst.to_rel(cur_reg)
-        out += "+"
+        out += "-" if neg else "+"
         cur_reg = dst
     out += src.to_rel(cur_reg)+"-]"
     out += root.to_rel(src)
@@ -209,20 +248,11 @@ class Program:
         return "\nend ]<<"
 
     def assemble_instruction(self, inst, cur_block_id):
-        scrap = reg(-1)
-        jmp = reg(-2)
+        scrap = Register(-1)
+        jmp = Register(-2)
         scrap2 = jmp
 
-        if isinstance(inst, Raw):
-            return inst.code
-        elif isinstance(inst, Output):
-            return (
-               f"out {inst.reg}\n    "
-              + inst.reg.to()
-              + "."
-              + inst.reg.back()
-            )
-        elif isinstance(inst, Jump):
+        if isinstance(inst, Jump):
             return (
                f"jmp {sanitize(inst.target)}\n    "
               + jmp.to()
@@ -232,6 +262,7 @@ class Program:
                 )
               + jmp.back()
             )
+
         elif isinstance(inst, JumpConditional):
             return (
                f"jnz {inst.cond} {sanitize(inst.target)}\n    "
@@ -251,6 +282,7 @@ class Program:
               + "]"
               + scrap.back()
             )
+
         elif isinstance(inst, JumpRelative):
             return (
                f"jmr {inst.offset}\n    "
@@ -258,30 +290,90 @@ class Program:
               + "+"*inst.offset
               + jmp.back()
             )
-        elif isinstance(inst, MovAdd):
+
+        elif isinstance(inst, Move):
+            header = f"mov {inst.dst} {inst.src}\n    "
+            if inst.src.is_immediate():
+                return (
+                    header
+                  + inst.dst.to()
+                  + "[-]"
+                  + "+"*inst.src
+                  + inst.dst.back()
+                )
+            else:
+                return (
+                     header
+                   + inst.dst.to()
+                   + "[-]"
+                   + move(inst.src, inst.dst, root=inst.dst)
+                   + inst.dst.back()
+                )
+        
+        elif isinstance(inst, Copy):
             return (
-               f"addm {inst.dst} {inst.src}\n    "
-             + move(inst.src, inst.dst)
+               f"cpy {inst.dst} {inst.src}\n    "
+              + inst.dst.to()
+              + "[-]"
+              + move(inst.src, scrap, inst.dst, root=inst.dst)
+              + move(scrap, inst.src, root=inst.dst)
+              + inst.dst.back()
             )
-        elif isinstance(inst, CopyAdd):
+
+        elif isinstance(inst, MovAdd):
+            header = f"addm {inst.dst} {inst.src}\n    "
+
+            if inst.src.is_immediate():
+                return (
+                    header
+                  + inst.dst.to()
+                  + "+"*inst.src
+                  + inst.dst.back()
+                )
+            else:
+                return (
+                   header
+                 + move(inst.src, inst.dst)
+                )
+
+        elif isinstance(inst, Add):
             return (
                f"add {inst.dst} {inst.src}\n    "
              + move(inst.src, inst.dst, scrap)
              + move(scrap, inst.src)
             )
-        elif isinstance(inst, SubI):
+
+        elif isinstance(inst, MovSub):
+            header = f"subm {inst.dst} {inst.src}\n    "
+
+            if inst.src.is_immediate():
+                return (
+                    header
+                  + inst.dst.to()
+                  + "-"*inst.src
+                  + inst.dst.back()
+                )
+            else:
+                return (
+                   header
+                 + move(inst.src, inst.dst, negative=True)
+                )
+
+        elif isinstance(inst, Sub):
             return (
-               f"subi {inst.reg} {inst.val}\n    "
-              + inst.reg.to()
-              + "-"*inst.val
-              + inst.reg.back()
+               f"sub {inst.dst} {inst.src}\n    "
+             + move(inst.src, inst.dst, scrap, negative=[1,0])
+             + move(scrap, inst.src)
             )
-        elif isinstance(inst, SetI):
+
+        elif isinstance(inst, Raw):
+            return inst.code
+
+        elif isinstance(inst, Output):
             return (
-               f"seti {inst.reg} {inst.val}\n    "
+               f"out {inst.reg}\n    "
               + inst.reg.to()
-              + "[-]"
-              + "+"*inst.val
+              + "."
               + inst.reg.back()
             )
 
@@ -311,47 +403,85 @@ class Program:
                 ))
               + self.program_epilogue())
 
+def parse(s):
+    insts = []
+    for line in s.split("\n"):
+        if line.isspace() or not line: continue
+        line = line.strip(" ")
+        if line.endswith(":"):
+            insts.append(LabelDefine(line[:-1]))
+            continue
+        if " " not in line:
+            mnemonic = line
+            args = []
+        else:
+            mnemonic = line[:line.find(" ")]
+            args_str = line[line.find(" "):].strip(" ")
+            args = []
+            for arg_s in args_str.split(" "):
+                arg = arg_s.strip()
+                if arg in regs:
+                    args.append(regs[arg])
+                    continue
+                if arg[0] == "<" and arg[-1] == ">":
+                    args.append(arg[1:-1])
+                    continue
+
+                if arg.startswith("0x"):
+                    imm = int(arg[2:], 16)
+                elif arg.startswith("0o"):
+                    imm = int(arg[2:], 8)
+                elif arg.startswith("0b"):
+                    imm = int(arg[2:], 2)
+                else:
+                    imm = int(arg)
+                args.append(Immediate(imm))
+
+        op = MNEMONICS[mnemonic]
+        op_args = op.__match_args__
+        op_fields = op.__dataclass_fields__
+        if len(args) != len(op_args):
+            raise ValueError(f"Wrong number of arguments for {mnemonic}, expected {len(op_args)}, got {len(args)}")
+
+        for op_arg, arg in zip(op_args, args):
+            tp = op_fields[op_arg].type
+            if tp is Register:
+                tps = ["register"]
+            elif tp is RegisterOrImmediate:
+                tps = ["register", "immediate"]
+            elif tp is int:
+                tps = ["immediate"]
+            elif tp is str:
+                tps = ["label"]
+            else:
+                raise NotImplementedError()
+
+            if isinstance(arg, Register):
+                arg_tp = "register"
+            elif isinstance(arg, Immediate):
+                arg_tp = "immediate"
+            elif isinstance(arg, str):
+                arg_tp = "label"
+
+            if arg_tp not in tps:
+                expected = " or ".join(tps)
+                raise ValueError(f"Wrong type for argument {op_arg} or {mnemonic}, expected: {expected}, got {arg_tp}")
+
+        insts.append(op(*args))
+    return insts
 
 if __name__ == "__main__":
-    # pseudocode:
-    # if False:
-    #   print("!")
-    # rep = 9
-    # n1 = 0
-    # n2 = 1
-    # while rep:
-    #   sum = 0
-    #   sum += n1
-    #   sum += n2
-    #   n1 = n2
-    #   n2 = sum
-    #   rep -= 1
-    # print(n1)
+    import sys
+    if len(sys.argv) != 3:
+        print("usage: asm in.cbf out.b", file=sys.stderr)
+        exit(1)
 
-    rep = reg(1)
-    n1 = reg(2)
-    n2 = reg(3)
-    sum = reg(4)
+    with open(sys.argv[1]) as f:
+        in_contents = f.read()
 
-    instructions = [
-        Jump("begin"),
-          SetI(n1, ord("!")),
-          Output(n1),
-        LabelDefine("begin"),
-          SetI(rep, 9),
-          SetI(n1, 0),
-          SetI(n2, 1),
-        LabelDefine("loop"),
-          SetI(sum, 0),
-          MovAdd(sum, n1),
-          CopyAdd(sum, n2),
-          SetI(n1, 0),
-          MovAdd(n1, n2),
-          MovAdd(n2, sum),
-          SubI(rep, 1),
-        JumpConditional(rep, "loop"),
-          Output(n1)
-    ]
-
+    instructions = parse(in_contents)
     prog = Program(instructions)
-    print(prog.assemble())
+    out_contents = prog.assemble()
+
+    with open(sys.argv[2], "w") as f:
+        f.write(out_contents)
