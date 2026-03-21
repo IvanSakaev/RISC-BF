faf9d0c030c9c4719239696e36a8e74b91a9567e
Ivan Sakaev <x-sakaev@yandex.ru>
2026-03-21 23:01:15 +0300

    Add OR instruction

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import subprocess
 
 for i in range(100):
     num1 = random.randrange(2**32)
-    num2 = random.randrange(0, 32)
+    num2 = random.randrange(2**32)
 
     with open("tests/t.s", "w") as file:
         file.write(
             f"""
 li x1, 0x{num1:x}
-slli x3, x1, 0x{num2:x}
-out x3
+li x2, 0x{num2:x}
+or x2, x1, x2
+out x2
 """.lstrip()
         )
 
@@ -36,12 +37,13 @@ out x3
         .rstrip("\n")
     )
 
-    num3 = num1 * (2**num2)
-    num3 %= 2**32
+    num3 = num1 | num2
     num3_str = f"{num3:08X}"
 
     if predict != num3_str:
-        print(f"num1: {num1:08X}\nnum2: {num2}\ncorrect: {num3_str}\npredicted: {predict}")
+        print(
+            f"num1: {num1:08X}\nnum2: {num2:08X}\ncorrect: {num3_str}\npredicted: {predict}"
+        )
         exit()
     if i % 10 == 0:
         print(f"tested {i} times")
