c56ddb356e226deaf3a76c413bf98b4ffdf4062d
Ivan Sakaev <x-sakaev@yandex.ru>
2026-04-19 20:04:16 +0300

    Fix recursion.s

--- examples/recursion.s
+++ examples/recursion.s
@@ -1,38 +1,37 @@
-factorial:
-	addi	sp, sp, -16
-	sw	ra, 12(sp)                      # 4-byte Folded Spill
-	sw	s0, 8(sp)                       # 4-byte Folded Spill
-	addi	s0, sp, 16
-	sw	a0, -16(s0)
-	lw	a0, -16(s0)
-	bnez	a0, LBB0_2
-	j	LBB0_1
-LBB0_1:
-	li	a0, 1
-	sw	a0, -12(s0)
-	j	LBB0_3
-LBB0_2:
-	lw	a0, -16(s0)
-	addi	a0, a0, -1
-	call	factorial
-	lw	a1, -16(s0)
-	mul	a0, a0, a1
-	sw	a0, -12(s0)
-	j	LBB0_3
-LBB0_3:
-	lw	a0, -12(s0)
-	lw	ra, 12(sp)                      # 4-byte Folded Reload
-	lw	s0, 8(sp)                       # 4-byte Folded Reload
-	addi	sp, sp, 16
-	ret
 _start:
-	addi	sp, sp, -16
-	sw	ra, 12(sp)                      # 4-byte Folded Spill
-	sw	s0, 8(sp)                       # 4-byte Folded Spill
-	addi	s0, sp, 16
-	li	a0, 3
-	call	factorial
-	lw	ra, 12(sp)                      # 4-byte Folded Reload
-	lw	s0, 8(sp)                       # 4-byte Folded Reload
-	addi	sp, sp, 16
-	ret
+    li a0, 5        # n = 5
+    jal ra, fact    # fact(n)
+
+    # результат уже в a0 -> возвращаем как код выхода
+    out a0
+    j last
+
+
+# fact(n)
+# a0 = n
+# return a0 = n!
+
+fact:
+    addi sp, sp, -16
+    sw ra, 12(sp)
+    sw a0, 8(sp)
+
+    li t0, 1
+    ble a0, t0, base_case
+
+    addi a0, a0, -1
+    jal ra, fact
+
+    lw t1, 8(sp)
+    mul a0, a0, t1
+    j end
+
+base_case:
+    li a0, 1
+
+end:
+    lw ra, 12(sp)
+    addi sp, sp, 16
+    ret
+last:
+    nop
