93fc8ee4bda1c0d3e3a88627b646aa9019e1f2e2
Ivan Sakaev <x-sakaev@yandex.ru>
2026-04-29 22:26:05 +0300

    Make compilation much faster

--- concater.py
+++ concater.py
@@ -11,7 +11,9 @@ if TYPE_CHECKING:
 class _Concater:
     def __init__(self, root: Cell):
         self.current_pos = root
-        self.current_program = ""
+        self.current_program = []
+        # Using a list and joining it at the end is much faster than adding string every
+        # time, because `a = a + "hello"` creates a new string instead of modifying current, which is very slow.
 
     @classmethod
     def sanitize(cls, name: str):
@@ -29,13 +31,13 @@ class _Concater:
         return name
 
     def raw(self, text: str, pos_offset: int = 0):
-        self.current_program += text
+        self.current_program.append(text)
         self.current_pos = self.current_pos.cell_rel(pos_offset)
 
     def rem(self, text: str, comments: bool):
         if comments:
             bp = " #" if config.BREAKPOINT_EVERY_INSTRUCTION else ""
-            self.current_program += "\n        " + self.sanitize(text) + bp + "\n          "
+            self.raw("\n        " + self.sanitize(text) + bp + "\n          ")
 
     def debug(self):
         self.raw("#")
@@ -44,9 +46,9 @@ class _Concater:
         self.raw(f"@{self.current_pos.addr:x}")
 
     def get_code(self):
-        return self.current_program
+        return "".join(self.current_program)
 
     def reset_code(self, new_pos: Cell | None = None):
-        self.current_program = ""
+        self.current_program = []
         if new_pos is not None:
             self.current_pos = new_pos
