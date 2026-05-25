f03b1bbf6b9acefcc88307ca7bd359c6fd83bef5
Ivan Sakaev <x-sakaev@yandex.ru>
2026-05-25 19:14:04 +0300

    Add -c option instead of COMPRESSED constant

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ PROGRAM_START_ADDRESS = 16  # program (not data) addresses start from PROGRAM_ST
 
 # Memory cell count will be 16^MEMORY_ADDRESS_HALFBYTES
 # MEMORY_ADDRESS_HALFBYTES > 3 may NOT work properly with most interpretators except ibf
-# MEMORY_ADDRESS_HALFBYTES > 5 will use too much RAM while generating file if COMPRESSED=False
+# MEMORY_ADDRESS_HALFBYTES > 5 will use too much RAM while generating file without option -c (compressing)
 # MEMORY_ADDRESS_HALFBYTES > 6 is not supported
-# If COMPRESSED=False, the output file size will be heavily dependent on this value
+# Without option -c (compressing), the output file size will be heavily dependent on this value
 MEMORY_ADDRESS_HALFBYTES = 6
 # This flag increases memory size by 16 times and doesn't increase the output file size.
 MEMORY_ADDRESS_LAST_HALFBYTE_AS_BYTE = True
@@ -22,10 +22,6 @@ PRELOAD_MEMORY = True
 ########## Next constants are useful only for ibf brainfuck interpretator. ##########
 # For other interpretators it's strictly recommended to disable them.
 # ibf interpretator: https://github.com/sit-itmo/DoomBF/tree/master/bf/industrial-bf
-
-# Save program in compressed format (for ibf with -c option)
-# If COMPRESSED=False, the output file size will be VERY BIG. Consider redusing MEMORY_ADDRESS_HALFBYTES.
-COMPRESSED = True
 
 # Using # symbol for breakpoints.
 ALLOW_DEBUG = True  # Allow breakpoints in concater.debug()
